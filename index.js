168af41f9cf95ece1bf6b327f44c5f0180865bc9
Sanskriti Harmukh <74777863+SanskritiHarmukh@users.noreply.github.com>
2024-09-18 18:53:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";  
+import bodyParser from "body-parser";// helps to parse the body of the request 
+import cors from "cors"; 
+import quotesRoutes from "./routes/quotes.js"; 
+  
+ const app = express(); 
+ app.use(cors()); 
+ const PORT = process.env.PORT || 5000; 
+  
+ app.use(bodyParser.json()); 
+  
+ app.use('/quotes', quotesRoutes); 
+  
+ app.get('/', (req, res) => {res.send("Hello World!")}); 
+  
+ app.listen(PORT, () => {console.log('Server is up and running!')});
