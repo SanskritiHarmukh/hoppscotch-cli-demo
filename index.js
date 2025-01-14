22f5a01330c8243a46827893a3972d33b120fd6f
SanskritiHarmukh <sanskritiharmukh1908@gmail.com>
2025-01-15 00:51:43 +0530

    Update: remove digest auth
    Signed-off-by: SanskritiHarmukh <sanskritiharmukh1908@gmail.com>

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 import express from "express";  
-import bodyParser from "body-parser"; 
+import bodyParser from "body-parser";// helps to parse the body of the request 
 import cors from "cors"; 
-import quotesRoutes from "./routes/quotes.js";  
-import httpAuth from "http-auth";
-import authConnect from "http-auth-connect"; 
-
-const app = express(); 
-app.use(cors()); 
-const PORT = process.env.PORT || 5000; 
-
-app.use(bodyParser.json()); 
-
-const digest = httpAuth.digest({
-    realm: "exampleRealm",
-    file: ".htdigest" 
-});
-
-app.use('/quotes', authConnect(digest), quotesRoutes); 
-
-app.get('/', authConnect(digest), (req, res) => { 
-    res.send(`Hello from express - ${req.user}!`); 
-}); 
-
-app.listen(PORT, () => { 
-    console.log(`Server is up and running on port ${PORT}!`); 
-});
+import quotesRoutes from "./routes/quotes.js"; 
+  
+ const app = express(); 
+ app.use(cors()); 
+ const PORT = process.env.PORT || 5000; 
+  
+ app.use(bodyParser.json()); 
+  
+ app.use('/quotes', quotesRoutes); 
+  
+ app.get('/', (req, res) => {res.send("Hello World!")}); 
+  
+ app.listen(PORT, () => {console.log('Server is up and running!')});
